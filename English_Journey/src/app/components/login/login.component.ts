bef3faf3ea8fdec9b0874bbcc5310aacd92c7af9
alfredo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-05-01 14:08:22 +0200

    Validaciones login

--- English_Journey/src/app/components/login/login.component.ts
+++ English_Journey/src/app/components/login/login.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderLoginComponent } from '../header-login/header-login.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderLoginComponent,],
+  imports: [HeaderLoginComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+ constructor(){}
+ loginform = new FormGroup({
+  mail : new FormControl('',[Validators.email, Validators.required]),
+  password : new FormControl('',Validators.required)
+ })
 
+ onSubmit(){
+  console.log("hola")
+ }
 }
