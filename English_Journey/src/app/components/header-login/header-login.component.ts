6ce45d5f79811969b120340d6cdbdc11ca276249
alfredo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-05-01 12:45:57 +0200

    Estilos login

---
+++ English_Journey/src/app/components/header-login/header-login.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-login.component.html',
+  styleUrl: './header-login.component.scss'
+})
+export class HeaderLoginComponent {
+
+
+}
--- English_Journey/src/app/components/login/login.component.ts
+++ English_Journey/src/app/components/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderLoginComponent } from '../header-login/header-login.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HeaderLoginComponent,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
