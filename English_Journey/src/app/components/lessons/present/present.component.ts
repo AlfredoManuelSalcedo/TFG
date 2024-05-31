072f330c62d0a7e6333ed014699e31c29b353d16
alfredo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-05-31 18:52:00 +0200

    Test base

--- English_Journey/src/app/components/lessons/present/present.component.ts
+++ English_Journey/src/app/components/lessons/present/present.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatRadioModule} from '@angular/material/radio';
+import {FormControl,FormBuilder} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-present',
   standalone: true,
-  imports: [],
+  imports: [MatStepperModule,MatRadioModule,RouterLink],
   templateUrl: './present.component.html',
   styleUrl: './present.component.scss'
 })
